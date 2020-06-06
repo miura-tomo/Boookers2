032acf0dfb5545056b0e8b8cc6acfb51e0b5f425
石黒　友哉 <ishiguro-tomoya@MacBook-Pro.lan>
2020-06-06 12:55:27 +0900

    [Add]コメント投稿・削除機能

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,7 +1,30 @@
 class BookCommentsController < ApplicationController
 def create
-end
-def destroy
+	@book = Book.find(params[:book_id])
+	@book_comment = current_user.book_comments.new(book_comment_params)
+	@book_comment.book_id = @book.id
+	@book_comment.user_id = current_user.id
+
+ if @book_comment.save
+    redirect_back(fallback_location: root_path)
+
+ else
+	@book = @post_book
+	@user = @book.user
+	render 'books/show'
+ end
+
 end
 
+
+def destroy
+	@book_comment = BookComment.find(params[:book_id])
+	@book_comment.destroy
+	redirect_back(fallback_location: root_path)
+end
+
+private
+	def book_comment_params
+		params.require(:book_comment).permit(:comment)
+	end
 end
